de4034adf0df7a3a053684193f83d0266f0fb377
vantonuk35 <65894352+vantonuk35@users.noreply.github.com>
2021-07-13 20:43:25 +0300

    Add files via upload

--- ScreenShoter.cpp
+++ ScreenShoter.cpp
@@ -1,9 +1,9 @@
 #include "ScreenShoter.h"
+
 cv::Mat DXScreenShoter11::ExtractBitmap(ID3D11Texture2D* d3dtex, ID3D11Device* pDevice)
 {
 	D3D11_TEXTURE2D_DESC desc;
-	ID3D11Texture2D* pNewTexture = NULL;
-
+	ID3D11Texture2D* pNewTexture = nullptr;
 	D3D11_TEXTURE2D_DESC description;
 
 	d3dtex->GetDesc(&desc);
@@ -14,7 +14,7 @@ cv::Mat DXScreenShoter11::ExtractBitmap(ID3D11Texture2D* d3dtex, ID3D11Device* p
 	description.Usage = D3D11_USAGE_STAGING;
 	description.MiscFlags = 0;
 
-	pDevice->CreateTexture2D(&description, NULL, &pNewTexture);
+	pDevice->CreateTexture2D(&description, nullptr, &pNewTexture);
 	ctx->CopyResource(pNewTexture, d3dtex);
 
 	D3D11_MAPPED_SUBRESOURCE resource;
@@ -22,10 +22,9 @@ cv::Mat DXScreenShoter11::ExtractBitmap(ID3D11Texture2D* d3dtex, ID3D11Device* p
 
 	// Copy from texture to bitmap buffer.
 	cv::Mat Texture = cv::Mat(desc.Height, desc.Width, CV_8UC4);
-	for (int i = 0; i < desc.Height; ++i)
-	{
-		memcpy(Texture.data + desc.Width * i * 4, (uchar*)resource.pData + desc.Width * i * 4, desc.Width * 4);
-	}
+
+	//memcpy(Texture.data , resource.pData , desc.Width * 4* desc.Height);
+	Texture.data = (uchar*)resource.pData;
 
 	ctx->Unmap(pNewTexture, 0);
 	pNewTexture->Release();
@@ -34,51 +33,41 @@ cv::Mat DXScreenShoter11::ExtractBitmap(ID3D11Texture2D* d3dtex, ID3D11Device* p
 }
 void DXScreenShoter11::Init()
 {
-
-
 	D3D_FEATURE_LEVEL d3dFeatLvl;
 	IDXGIFactory1* pFactory;
 	IDXGIAdapter1* pAdapter;
 	IDXGIOutput* pOutput;
 	IDXGIOutput1* pOutput1;
 
+	CreateDXGIFactory1(__uuidof(IDXGIFactory1), (void**)(&pFactory));
 
-	hr = CreateDXGIFactory1(__uuidof(IDXGIFactory1), (void**)(&pFactory));
+	pFactory->EnumAdapters1(0, &pAdapter);
+	pAdapter->EnumOutputs(0, &pOutput);
 
-	hr = pFactory->EnumAdapters1(0, &pAdapter);
+	pOutput->QueryInterface(__uuidof(IDXGIOutput1), reinterpret_cast<void**>(&pOutput1));
 
-	hr = pAdapter->EnumOutputs(0, &pOutput);
-
-	hr = pOutput->QueryInterface(__uuidof(IDXGIOutput1), reinterpret_cast<void**>(&pOutput1));
-
-	hr = D3D11CreateDevice(pAdapter, D3D_DRIVER_TYPE_UNKNOWN,
+	D3D11CreateDevice(pAdapter, D3D_DRIVER_TYPE_UNKNOWN,
 		0, 0, 0, 0,
 		D3D11_SDK_VERSION,
 		&pDevice,
 		&d3dFeatLvl,
 		&pImmediateContext);
-	hr = pOutput1->DuplicateOutput(pDevice, &pDeskDupl);
+	pOutput1->DuplicateOutput(pDevice, &pDeskDupl);
 
 	pDevice->GetImmediateContext(&ctx);
-
 };
 cv::Mat DXScreenShoter11::Take()
 {
 	DXGI_OUTDUPL_FRAME_INFO FrameInfo;
-	//You need it, because other way there would be black frames
-	Sleep(1);
 	// Get new frame.
 	hr = pDeskDupl->AcquireNextFrame(0, &FrameInfo, &DesktopResource);
 	if (FAILED(hr))
-	{
 		return cv::Mat(1080, 1920, CV_8UC4);
-	}
 
-	hr = DesktopResource->QueryInterface(__uuidof(ID3D11Texture2D), reinterpret_cast<void**>(&AcquiredDesktopImage));
+	DesktopResource->QueryInterface(__uuidof(ID3D11Texture2D), reinterpret_cast<void**>(&AcquiredDesktopImage));
 	DesktopResource->Release();
 
-	cv::Mat hBmp =
-		ExtractBitmap(AcquiredDesktopImage, pDevice);
-	hr = pDeskDupl->ReleaseFrame();
+	cv::Mat hBmp = ExtractBitmap(AcquiredDesktopImage, pDevice);
+	pDeskDupl->ReleaseFrame();
 	return hBmp;
 }
--- Source.cpp
+++ Source.cpp
@@ -15,13 +15,13 @@
 #include <string>
 #include <windows.h>
 #include <shlobj.h>
-
+#include <future>
 #include "ScreenShoter.h"
 auto get_time()
 {
 	return std::chrono::duration_cast<std::chrono::milliseconds>(
 		std::chrono::system_clock::now().time_since_epoch()
-		);
+	);
 }
 
 
@@ -29,13 +29,17 @@ auto get_time()
 class DetectingArrow
 {
 	std::vector<std::pair<cv::Scalar, cv::Scalar>> _filters;//First - low_pass ; Second - High_pass
+	int _arrow_num;
 public:
-	DetectingArrow(std::vector<std::pair<cv::Scalar, cv::Scalar>> Filters)
+	DetectingArrow(std::vector<std::pair<cv::Scalar, cv::Scalar>> Filters, int arrow_num)
 	{
 		_filters = Filters;
+		_arrow_num = arrow_num;
 	}
-	cv::Mat FilterImage(cv::Mat Input)
+	cv::Mat FilterImage(cv::Mat Input, int downscale_factor)
 	{
+		cv::Rect new_size = { _arrow_num * 160 / downscale_factor,0,160 / downscale_factor ,Input.rows };
+		Input = Input(new_size);
 		cv::Mat Additional, Output;
 		Output = cv::Mat::zeros(Input.rows, Input.cols, CV_8UC1);
 
@@ -64,27 +68,27 @@ class ArrowHandler
 			DetectingArrow({
 				{cv::Scalar(147, 147, 194), cv::Scalar(165, 161, 206)},
 				{cv::Scalar(150, 85, 165), cv::Scalar(155, 95, 175)}
-			})
+			},0)
 		},
 		{
 			ARR_RIGHT,
 			DetectingArrow({
 				{cv::Scalar(172, 187, 243), cv::Scalar(187, 205, 253)},
 				{cv::Scalar(170, 125, 200), cv::Scalar(180, 135, 210)}
-			})
+			},3)
 		},
 		{
 			ARR_UP,
 			DetectingArrow({
 				{cv::Scalar(56, 236, 246), cv::Scalar(59, 255, 253)},
 				{cv::Scalar(60, 175, 210), cv::Scalar(65, 185, 220)}
-			})
+			},2)
 		},
 		{
 			ARR_DOWN,
 			DetectingArrow({
 				{cv::Scalar(85, 245, 255), cv::Scalar(95, 255, 255)}, {cv::Scalar(87, 185, 215), cv::Scalar(108, 195, 225)}
-			})
+			},1)
 		}
 	};
 	int TrashHold;
@@ -95,29 +99,26 @@ class ArrowHandler
 	}
 
 public:
-	ArrowHandler(int new_trashhold) : TrashHold(new_trashhold)
-	{
-	};
+	ArrowHandler(int new_trashhold) : TrashHold(new_trashhold){};
 
-	void ProcessImage(cv::Mat Input, HWND gameWindow)
+	void ProcessImage(cv::Mat &Input, HWND gameWindow,int downscale_factor)
 	{
 		for (auto& [arrow_id, arrow_instance] : Arrows)
 		{
-			SendOutput(arrow_instance.FilterImage(Input), gameWindow, arrow_id);
+			SendOutput(arrow_instance.FilterImage(Input, downscale_factor), gameWindow, arrow_id);
 		}
 	}
 };
 
-cv::Mat Cut_screenshot_to_arrow_zone(cv::Mat Input, int downscale_factor, HWND game_window)
-{
+void Cut_screenshot_to_arrow_zone(cv::Mat &Input, int downscale_factor, HWND game_window)
+{ 
 	RECT gameRect;
 	GetWindowRect(game_window, &gameRect);
-	Input = Input({ gameRect.left, gameRect.top, gameRect.right - gameRect.left, gameRect.bottom - gameRect.top });
-	cv::resize(Input, Input,
-		{ 1920 / downscale_factor, 1080 / downscale_factor });
-	Input = Input({ 1920 / downscale_factor / 2, 30,
-		1920 / downscale_factor / 2, 1080 / downscale_factor / 4 - 50 });
-	return Input;
+	Input = Input({gameRect.left, gameRect.top, gameRect.right - gameRect.left, gameRect.bottom - gameRect.top});
+	cv::resize(Input, Input, 
+		{1920 / downscale_factor, 1080 / downscale_factor});
+	Input = Input({1920 / downscale_factor / 2+int(100/downscale_factor), int(120/downscale_factor),
+		1920 / downscale_factor / 2-int(310/downscale_factor), 1080 / downscale_factor / 4 - int(160/downscale_factor)});
 }
 
 int main()
@@ -126,18 +127,19 @@ int main()
 	DXScreenShoter11 screen_shot_manager;
 	screen_shot_manager.Init();
 
-
-	int down_scale = 4;
-	int pixel_reaction_sum = 20000 / down_scale / down_scale;
+	int down_scale = 8;
+	int pixel_reaction_sum = 40000 / down_scale / down_scale;
 	ArrowHandler arrow_handler(pixel_reaction_sum);
-	int key;
 	while ((game_window = FindWindowA(NULL, "Friday Night Funkin'")))
 	{
+		static auto last_time = get_time();
+		std::cout << (get_time() - last_time).count() << "\t milliseconds " << '\n';
+		last_time = get_time();
 		cv::Mat src = screen_shot_manager.Take();
 		cv::Mat hsv_src;
-		cv::cvtColor(Cut_screenshot_to_arrow_zone(src, down_scale, game_window), hsv_src, cv::COLOR_BGR2HSV);
-		arrow_handler.ProcessImage(hsv_src, game_window);
-		key = cv::waitKey(1);
+		Cut_screenshot_to_arrow_zone(src, down_scale, game_window);
+		cv::cvtColor(src, hsv_src, cv::COLOR_BGR2HSV);
+		arrow_handler.ProcessImage( hsv_src, game_window, down_scale);
 	}
 	return 0;
 }
